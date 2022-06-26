b1e18e71cd9b398d7c8c47512b8fa75236e236be
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-25 23:26:22 -0700

    added the annotations for better understanding in future

--- src/CheckTheSum.java
+++ src/CheckTheSum.java
@@ -2,13 +2,18 @@ import java.util.HashMap;
 
 public class CheckTheSum {
     public static boolean checkTheSum(int []arr, int n, int x){
+        //start storing it in hashmap
         HashMap<Integer, Boolean> map = new HashMap<>();
+        //run a loop until n
         for(int i = 0; i<n; i++){
+            //if map contains other portion that adds to make x then return true
             if(map.containsKey(x-arr[i])){
                 return true;
+                //otherwise just put everything in map
             }else{
                 map.put(arr[i], true);
             }
+            //else if no condition matches, or array is empty return false
         }return false;
 
     }
