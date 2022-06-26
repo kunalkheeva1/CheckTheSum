c3b769aaea1e3a8d99af3fd2f7b1f11d2bb25319
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-25 23:24:25 -0700

    created a method to check if array contains any other element that adds to first number so that they can be equal to x

--- src/CheckTheSum.java
+++ src/CheckTheSum.java
@@ -1,3 +1,18 @@
+import java.util.HashMap;
+
 public class CheckTheSum {
-    
+    public static boolean checkTheSum(int []arr, int n, int x){
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        for(int i = 0; i<n; i++){
+            if(map.containsKey(x-arr[i])){
+                return true;
+            }else{
+                map.put(arr[i], true);
+            }
+        }return false;
+
+    }
+    public static void main(String[] args) {
+
+    }
 }
